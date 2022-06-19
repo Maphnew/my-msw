6c7271c7089e679265099b309e213ad4bc0c60ad
maphnew <zcm3118@gmail.com>
2022-06-19 18:38:33 +0900

    Set up MSW

---
+++ src/mocks/handlers.js
@@ -0,0 +1,13 @@
+import { rest } from "msw";
+
+const todos = ["먹기", "자기", "놀기"];
+
+export const handlers = [
+  rest.get("/todos", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(todos));
+  }),
+  rest.post("/todos", (req, res, ctx) => {
+    todos.push(req.body);
+    return res(ctx.status(201));
+  }),
+];
